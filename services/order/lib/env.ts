c93cc8eb28123946aa240b869f3ff606a8bff6c8
shahreaz0 <ashahreaz@gmail.com>
2024-11-03 14:30:32 +0600

    feat: setup env

--- services/order/lib/env.ts
+++ services/order/lib/env.ts
@@ -4,7 +4,9 @@ const _env = {
   PORT: process.env.PORT,
   SERVICE_NAME: process.env.SERVICE_NAME,
   DATABASE_URL: process.env.DATABASE_URL,
-  INVENTORY_BASE_URL: process.env.INVENTORY_BASE_URL,
+  PRODUCT_SERVICE_BASE_URL: process.env.PRODUCT_SERVICE_BASE_URL,
+  EMAIL_SERVICE_BASE_URL: process.env.EMAIL_SERVICE_BASE_URL,
+  CART_SERVICE_BASE_URL: process.env.CART_SERVICE_BASE_URL,
 };
 
 export const env = {
